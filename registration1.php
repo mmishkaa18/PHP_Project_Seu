dba508dd75157f72b1bffecd27c8cd4d73930d78
Mishka Khoshebaevi <mkhoshebaevi1@seu.edu.ge>
2026-01-08 17:19:08 +0400

    register page

--- registration1.php
+++ registration1.php
@@ -1,9 +1,10 @@
-<!-- <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="registration.css">
+    <link rel="stylesheet" href="registration1.css">
+    <link rel="stylesheet" href=".css">
     <link rel="shortcut icon" type="image/x-icon" href="images/f1-default-share.jpg"/>
     <title>F1 Registration</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -31,8 +32,8 @@
     <div class="registration-form">
         <form action="drivers.php">
             <div class="input-box">
-                <p>Title</p>
-                <select name="Select" id="">
+                <p class="input-p">Title</p>
+                <select name="Select" class="input">
                     <option value="">Select</option>
                     <option value="">Mr</option>
                     <option value="">Mrs</option>
@@ -41,20 +42,20 @@
                 </select>
             </div>
             <div class="input-box">
-                <p>First Name</p>
-                <input type="text" placeholder="First Name" id="">
+                <p class="input-p">First Name</p>
+                <input type="text" placeholder="First Name" class="input">
             </div>
             <div class="input-box">
-                <p>Last Name</p>
-                <input type="text" placeholder="Last Name" id="">
+                <p class="input-p">Last Name</p>
+                <input type="text" placeholder="Last Name" class="input">
             </div>
             <div class="input-box">
-                <p>Date of birth</p>
-                <input type="date" id="">
+                <p class="input-p">Date of birth</p>
+                <input type="date" class="input">
             </div>
             <div class="input-box">
-                <p>Country of residence</p>
-                <select name="" required>
+                <p class="input-p">Country of residence</p>
+                <select name="" required class="input">
                     <option value="">Select</option>
 
                     <?php while ($row = mysqli_fetch_assoc($result)) { ?>
@@ -65,14 +66,14 @@
                 </select>
             </div>
             <div class="input-box">
-                <p>Email address</p>
-                <input type="text" placeholder="Email address" id="">
+                <p class="input-p">Email address</p>
+                <input type="text" placeholder="Email address" class="input">
             </div>
             <div class="input-box">
-                <p>Password</p>
-                <input type="password" placeholder="********" id="">
+                <p class="input-p">Password</p>
+                <input type="password" placeholder="********" class="input">
             </div>
-            <div class="input-box">
+            <div class="input-register">
                 <button>Register</button>
             </div>
          </form>                
@@ -82,4 +83,4 @@
 </body>
 </html>
 
- -->
+
