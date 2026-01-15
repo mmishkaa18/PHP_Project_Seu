5ee2ae6621838180f1e0155ad8542f6f2c2da541
Mishka Khoshebaevi <mkhoshebaevi1@seu.edu.ge>
2026-01-15 17:43:33 +0400

    designing drivers page/connecting to base

---
+++ driverStatistics.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>F1 Drivers statistics</title>
+</head>
+<body>
+    <header class="site-header">
+  <nav class="navbar">
+    <div class="nav-inner">
+
+      <a href="#" class="nav-logo">
+        <img src="images/F1.svg.png" alt="F1 Logo">
+      </a>
+
+      <ul class="nav-links">
+        <li><a href="#">Schedule</a></li>
+        <li><a href="#">Results</a></li>
+        <li><a href="#">News</a></li>
+        <li class="active"><a href="#">Drivers</a></li>
+        <li><a href="#">Teams</a></li>
+        <li><a href="#">Fantasy & Gaming</a></li>
+        <li><a href="#">F1 Members' Area</a></li>
+        <li class="registration">
+          <a href="registration1.php">Registration</a>
+        </li>
+      </ul>
+
+    </div>
+  </nav>
+  </header>
+</body>
+</html>
